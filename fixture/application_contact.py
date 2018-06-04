cf6625f1081e8294f2cded9ad5df6fa7f8b54315
Ольга <Olechka56741@gmail.com>
2018-06-04 22:26:39 +0300

    Выделен класс для управления сессиями

--- fixture/application_contact.py
+++ fixture/application_contact.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -7,21 +8,11 @@ class Application:
         self.wd = WebDriver(capabilities={"marionette": False},
                             firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_add_new_page(self):
         wd = self.wd
@@ -89,10 +80,6 @@ class Application:
         wd = self.wd
         self.submit_group_creation()
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,10 +12,10 @@ def app(request):
 
 
 def test_test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.feel_address_book_entry_form(Contact(firstname="Olga", middlename="new", lastname="Urushkina", nickname="Redfox",
                                       title="company", company="ipder", address="jjjjj",
                                       home="1111111111", mobile="2222222222", work="3333333333", fax="8888888888",
                                       email="data@rambler.ru", email2="few@mail.com", email3="hpme@yamdex.ru",
                                       homepage="www.ggg.net", address2="rrrrr", phone2="yyyyy", notes="bbbbb"))
-    app.logout()
+    app.session.logout()

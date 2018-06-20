ce1abfa1e99ec148b8c8cdb09682e3d89eb6e5bd
Ольга <Olechka56741@gmail.com>
2018-06-20 03:48:12 +0300

    Добавлено обеспечение выполнения предусловий

--- fixture/group.py
+++ fixture/group.py
@@ -58,3 +58,9 @@ class GroupHelper:
         self.return_to_groups_page()
         wd.find_element_by_link_text("group page").click()
 
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+

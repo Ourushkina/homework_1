f0d96e7625ddcaaa39c3b61f2743d1d7d07c8005
Ольга <Olechka56741@gmail.com>
2018-07-06 20:56:08 +0300

    Реализована проверка равенства объектов и списков для тестов модификации групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,11 +2,15 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-   old_contacts = app.contact.get_contact_list()
-   #print(len(old_contacts))
-   if app.contact.count() == 0:
-       app.contact.create(Contact(firstname="test"))
-   app.contact.modify_first_contact(Contact(firstname="New contact"))
-   new_contacts = app.contact.get_contact_list()
-   #print(len(new_contacts))
-   assert len(old_contacts) == len(new_contacts)
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="New contact")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    print(old_contacts)
+    print(new_contacts)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)

d8fd0d1aec5b0d42301a7f14eb49077574e95a56
Ольга <Olechka56741@gmail.com>
2018-07-06 15:07:41 +0300

    Реализована проверка равенства объектов и списков для тестов удаления групп и контакотов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,10 +3,10 @@ from model.contact import Contact
 
 def test_modify_contact_firstname(app):
    old_contacts = app.contact.get_contact_list()
-   print(len(old_contacts))
+   #print(len(old_contacts))
    if app.contact.count() == 0:
        app.contact.create(Contact(firstname="test"))
    app.contact.modify_first_contact(Contact(firstname="New contact"))
    new_contacts = app.contact.get_contact_list()
-   print(len(new_contacts))
+   #print(len(new_contacts))
    assert len(old_contacts) == len(new_contacts)

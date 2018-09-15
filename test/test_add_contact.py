34b071a195f358893d77e9bf126b374c5e5632a9
olga <Olechka56741@gmail.com>
2018-09-15 18:17:24 +0300

    Параметризованы тесты для создания контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,41 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_name(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_address(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_phone(prefix, maxlen):
+    symbols = string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_email(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Contact(firstname=random_name("firstname", 10), lastname=random_name("lastname", 10), address=random_name("address", 10),
+                      home=random_phone("+7", 10), mobile=random_phone("+7", 10), work=random_phone("+7", 10), phone2=random_phone("+7", 10),
+                      email=random_email("e1", 10), email2=random_email("e2", 10), email3=random_email("e3", 10))
+    for i in range(2)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Olga", lastname="Urushkina", address="Moscow",
-                      home="1111111111", mobile="2222222222", work="3333333333", phone2="8888888888",
-                      email="data@rambler.ru", email2="few@mail.com", email3="hpme@yamdex.ru")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()

d2b217101501416e12300ea98c871bcb7df7e2c2
Ольга <Olechka56741@gmail.com>
2018-09-16 21:11:03 +0300

    Реализована генерация тестовых данных для групп

--- generator/group.py
+++ generator/group.py
@@ -1,12 +1,26 @@
 from model.group import Group
 import random
 import string
+import os.path
+import json
+import getopt
+import sys
 
 
-constant = [
-    Group(name="name1", header="header1", footer="footer1"),
-    Group(name="name2", header="header2", footer="footer2")
-]
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/groups.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
 
 
 def random_string(prefix, maxlen):
@@ -16,7 +30,10 @@ def random_string(prefix, maxlen):
 
 testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
+    for i in range(n)
 ]
 
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))

bc425ee092a7bbc47735e7d8f7b8906627c7a26c
Ольга <Olechka56741@gmail.com>
2018-09-16 22:13:51 +0300

    Реализована генерация тестовых данных для контактов, сохраняющая созданные данные в файл в формате JSON

---
+++ generator/contact.py
@@ -0,0 +1,59 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 2
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_name(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_address(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_phone(prefix, maxlen):
+    symbols = string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_email(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Contact(firstname=random_name("firstname", 10), lastname=random_name("lastname", 10),
+            address=random_name("address", 10), home=random_phone("+7", 10), mobile=random_phone("+7", 10),
+            work=random_phone("+7", 10), phone2=random_phone("+7", 10), email=random_email("e1", 10),
+            email2=random_email("e2", 10), email3=random_email("e3", 10))
+    for i in range(n)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,37 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-
-def random_name(prefix, maxlen):
-    symbols = string.ascii_letters
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_address(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_phone(prefix, maxlen):
-    symbols = string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_email(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-    Contact(firstname=random_name("firstname", 10), lastname=random_name("lastname", 10),
-            address=random_name("address", 10), home=random_phone("+7", 10), mobile=random_phone("+7", 10),
-            work=random_phone("+7", 10), phone2=random_phone("+7", 10),email=random_email("e1", 10),
-            email2=random_email("e2", 10), email3=random_email("e3", 10))
-    for i in range(2)
-]
+from data.add_contact import testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])

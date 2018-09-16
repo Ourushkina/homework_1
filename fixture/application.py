2c61962a46d984fca23544257796f84c1ec6107e
Ольга <Olechka56741@gmail.com>
2018-09-16 12:46:09 +0300

    Реализованы опции командной строки. Тесты запускаются на Firefox, но не работают через Chrome и Ie

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -6,15 +6,22 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-       # self.wd = WebDriver(capabilities={"marionette": False},
-                            #firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
-        self.wd = WebDriver(capabilities={"marionette": False},
-                            firefox_binary="/usr/local/bin/firefox")
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False},
+                                        firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
+                                        # firefox_binary="/usr/local/bin/firefox"
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -25,8 +32,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("https://localhost/addressbook/index.php")
-        #wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
-       self.wd.quit()
+        self.wd.quit()

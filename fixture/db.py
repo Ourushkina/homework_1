7bbbd2da9b547da695643c757a36a3e7b53581d3
Ольга <Olechka56741@gmail.com>
2018-09-21 21:04:39 +0300

    Переделаны тесты на добавление групп и контактов, чтобы сравнивались данные, загруженные из БД

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,6 @@
 import pymysql.cursors
+from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -9,6 +11,34 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+        self.connection.autocommit(True)
+
+    def get_group_list(self):
+        group_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return group_list
+
+    def get_contact_list(self):
+        contact_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, phone2, email, email2, "
+                           "email3 from addressbook")
+            for row in cursor:
+                (id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3) = row
+                contact_list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address,
+                                            home=home, mobile=mobile, work=work, phone2=phone2, email=email,
+                                            email2=email2, email3=email3))
+        finally:
+            cursor.close()
+        return contact_list
 
     def destroy(self):
         self.connection.close()
